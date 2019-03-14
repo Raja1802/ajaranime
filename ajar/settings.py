2c18553ab412ffe508f54449e18940faba5d8a15
raja <rajagandla16@gmail.com>
2019-03-14 23:51:08 +0530

    make it better

--- ajar/settings.py
+++ ajar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'c+8h+z^xo4*0g8b0s8qok3izf1%0d5++phw*91bf3a=s9_v3qb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
